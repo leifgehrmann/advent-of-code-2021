446c0775803a98ce11e40ccd743d45b59145eeac
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-11-25 21:16:14 +0000

    Test that file-reading is possible both on local and in GitHub actions + GitHub action path watcher

--- Day-01-LoremIpsum.swift
+++ Day-01-LoremIpsum.swift
@@ -1,8 +1,24 @@
 import Foundation
 
+enum ScriptError: Error {
+    case EnvironmentPathNotDefined
+    case FileCouldNotBeRead
+}
+
 @main
 enum Script {
-    static func main() {
+    static func main() throws {
         print("Hello Xcode (Day 1)")
+        guard let srcRoot = ProcessInfo.processInfo.environment["SRCROOT"] else {
+            throw ScriptError.EnvironmentPathNotDefined
+        }
+        let inputPath = URL(fileURLWithPath: srcRoot + "/Day-01-Input.txt")
+        var input = ""
+        do {
+            input = try String(contentsOf: inputPath, encoding: .utf8)
+        } catch {
+            throw ScriptError.FileCouldNotBeRead
+        }
+        print(input)
     }
 }
