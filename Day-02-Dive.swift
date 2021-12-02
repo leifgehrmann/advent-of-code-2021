dc513c94a68e10947b00b193c6ca1e84e001bad3
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-12-02 06:46:32 +0000

    Day-02 - Part 2 of the puzzle solved!

--- Day-02-Dive.swift
+++ Day-02-Dive.swift
@@ -20,24 +20,71 @@ func readFileInCwd(file: String) throws -> String {
     }
 }
 
+func part1(commandTuples: [(direction: String, amount: Int)]) -> Int {
+    let horizontalSum = commandTuples
+        .filter { $0.direction == "forward" }
+        .reduce(0) {$1.amount + $0 }
+    let verticalSum = commandTuples
+        .filter { $0.direction == "down" || $0.direction == "up" }
+        .reduce(0) { ($1.amount) * ($1.direction == "up" ? -1 : 1) + $0}
+    
+    print("Vertical positiion: ", verticalSum)
+    print("Horizontal position: ", horizontalSum)
+    return verticalSum * horizontalSum
+}
+
+struct SubmarineState {
+    var aim: Int;
+    var horizontal: Int;
+    var vertical: Int;
+}
+
+func part2(commandTuples: [(direction: String, amount: Int)]) -> Int {
+    let submarineState = commandTuples.reduce(
+        SubmarineState(aim: 0, horizontal: 0, vertical: 0)
+    ) {
+        (
+            accumulation: SubmarineState,
+            command: (direction: String, amount: Int)
+        ) -> SubmarineState in
+
+        if command.direction == "down" || command.direction == "up" {
+
+            // Adjust the aim
+            let directionScalar = (command.direction == "up" ? -1 : 1)
+            return SubmarineState(
+                aim: accumulation.aim + command.amount * directionScalar,
+                horizontal: accumulation.horizontal,
+                vertical: accumulation.vertical
+            )
+
+        } else {
+
+            // We're going forward, so adjust the position
+            return SubmarineState(
+                aim: accumulation.aim,
+                horizontal: accumulation.horizontal + command.amount,
+                vertical: accumulation.vertical + accumulation.aim * command.amount
+            )
+
+        }
+
+    }
+    print(submarineState)
+    return submarineState.horizontal * submarineState.vertical
+}
+
 @main
 enum Script {
     static func main() throws {
         let input = try readFileInCwd(file: "/Day-02-Input.txt")
         
-        let commandStrings = input.split(separator: "\n")
-        let commandTupleStrings = commandStrings.map { $0.split(separator: " ") }
-        let commandTuples = commandTupleStrings.map { ($0[0], Int($0[1])!) }
+        let commandTuples = input
+            .split(separator: "\n") // Split by line
+            .map { $0.split(separator: " ") } // Split by space
+            .map { (direction: String($0[0]), amount: Int($0[1])!) } // Typedef commands
         
-        let horizontalSum = commandTuples
-            .filter { $0.0 == "forward" }
-            .reduce(0) {$1.1 + $0 }
-        let verticalSum = commandTuples
-            .filter { $0.0 == "down" || $0.0 == "up" }
-            .reduce(0) { ($1.1) * ($1.0 == "up" ? -1 : 1) + $0}
-        
-        print("Vertical positiion: ", verticalSum)
-        print("Horizontal position: ", horizontalSum)
-        print("Puzzle solution: ", horizontalSum * verticalSum)
+        print("Part 1: ", part1(commandTuples: commandTuples))
+        print("Part 2: ", part2(commandTuples: commandTuples))
     }
 }
