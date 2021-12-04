51cd29464405e23f30d2f843bafbbc7352f881ba
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-12-04 10:31:29 +0000

    Day-04 - Part 2 of the puzzle solved!

--- Day-04-Giant-Squid.swift
+++ Day-04-Giant-Squid.swift
@@ -32,11 +32,11 @@ class Board {
         self.marked = numbers.map { $0.map { _ in false }}
     }
     
-    func check(drawNumber: Int) -> Bool {
-        for (rowIndex, row) in self.numbers.enumerated() {
+    func mark(drawNumber: Int) -> Bool {
+        for (rowIndex, row) in numbers.enumerated() {
             for (colIndex, col) in row.enumerated() {
                 if col == drawNumber {
-                    self.marked[rowIndex][colIndex] = true
+                    marked[rowIndex][colIndex] = true
                     return true
                 }
             }
@@ -44,23 +44,43 @@ class Board {
         return false
     }
     
-    func checkBingo() -> Bool {
+    func hasBingo() -> Bool {
         // Check rows
-        for row in self.marked {
+        for row in marked {
             if (row.allSatisfy { $0 }) {
                 return true
             }
         }
         
         // Check cols
-        let colLength = self.marked[0].count
+        let colLength = marked[0].count
         for colIndex in 0..<colLength {
-            let transposedCol = self.marked.map {$0[colIndex]}
+            let transposedCol = marked.map {$0[colIndex]}
             if (transposedCol.allSatisfy { $0 }) {
                 return true
             }
         }
         return false
+    }
+    
+    func printBoardNumbers() {
+        let stringifiedNumbers = numbers.map { $0.map {
+            String($0).padding(toLength: 3, withPad: " ", startingAt: 0)
+        }}
+        let stringifiedRows = stringifiedNumbers.map {
+            $0.reduce("") { $0 + $1 }
+        }
+        print(stringifiedRows.reduce("") { $0 + String($1) + "\n" })
+    }
+
+    func printBoardMarked() {
+        let stringifiedMarked = marked.map { $0.map {
+            $0 ? "☑️ " : "⬜️ "
+        }}
+        let stringifiedRows = stringifiedMarked.map {
+            $0.reduce("") { $0 + $1 }
+        }
+        print(stringifiedRows.reduce("") { $0 + String($1) + "\n" })
     }
 }
 
@@ -88,20 +108,28 @@ func readPuzzle(from input: String) -> (drawNumbers: [Int], boards: [Board]) {
     return (drawNumbers: drawNumbers, boards: boards)
 }
 
+func getSolution(lastDrawNumber: Int, board: Board) -> Int {
+    let unmarkedMarkerSum = zip(
+        board.numbers.flatMap({$0}),
+        board.marked.flatMap({$0})
+    )
+        // Name the tuple, just for readability
+        .map {(number: $0.0, marked: $0.1)}
+        // Sum up all the unmarked numbers
+        .reduce(0) { $0 + (!$1.marked ? $1.number : 0) }
+    board.printBoardNumbers()
+    board.printBoardMarked()
+    print("Marked Sum: ", unmarkedMarkerSum)
+    print("Last Draw Number: ", lastDrawNumber)
+    return unmarkedMarkerSum * lastDrawNumber
+}
+
 func part1(_ puzzle: (drawNumbers: [Int], boards: [Board])) throws -> Int {
     for drawNumber in puzzle.drawNumbers {
         for board in puzzle.boards {
-            let marked = board.check(drawNumber: drawNumber)
-            
-            if marked, board.checkBingo() {
-                let unmarkedMarkerSum = zip(
-                    board.numbers.flatMap({$0}),
-                    board.marked.flatMap({$0})
-                )
-                    // Name the tuple, just for readability
-                    .map {(number: $0.0, marked: $0.1)}
-                    .reduce(0) { $0 + ($1.marked ? $1.number : 0) }
-                return unmarkedMarkerSum * drawNumber
+            let marked = board.mark(drawNumber: drawNumber)
+            if marked, board.hasBingo() {
+                return getSolution(lastDrawNumber: drawNumber, board: board)
             }
             
         }
@@ -109,12 +137,34 @@ func part1(_ puzzle: (drawNumbers: [Int], boards: [Board])) throws -> Int {
     throw ScriptError.NoBingo
 }
 
+func part2(_ puzzle: (drawNumbers: [Int], boards: [Board])) throws -> Int {
+    let numberOfBoards = puzzle.boards.count
+    var numberOfBingos = 0
+    for drawNumber in puzzle.drawNumbers {
+        for board in puzzle.boards {
+            if board.hasBingo() {
+                continue
+            }
+            
+            let marked = board.mark(drawNumber: drawNumber)
+            if marked, board.hasBingo() {
+                numberOfBingos += 1
+                if numberOfBingos == numberOfBoards {
+                    return getSolution(lastDrawNumber: drawNumber, board: board)
+                }
+            }
+        }
+    }
+    
+    throw ScriptError.NoBingo
+}
+
 @main
 enum Script {
     static func main() throws {
         let input = try readFileInCwd(file: "/Day-04-Input.txt")
-        let puzzle = readPuzzle(from: input)
-        
-        print(try part1(puzzle))
+
+        print("Part 1: ", try part1(readPuzzle(from: input)), "\n")
+        print("Part 2: ", try part2(readPuzzle(from: input)))
     }
 }
