7747a6e53318d62ec2523551eb86141bb9e5b46c
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-12-16 11:53:01 +0000

    Improve variable names and documentation

--- Day-16-Packet-Decoder.swift
+++ Day-16-Packet-Decoder.swift
@@ -1,8 +1,11 @@
 import Foundation
 
-/// A sequence of zeros and ones
+/// A sequence of zeros and ones.
 typealias Bits = [UInt8]
 
+/// The final parsed result. Note that each packet contains a list of
+/// sub-packets, or it optionally includes a literalValue. In practice
+/// a packet can only have one or the other.
 struct Packet {
     let version: Int;
     let typeId: Int;
@@ -10,20 +13,22 @@ struct Packet {
     let subPackets: [Packet]
 }
 
+/// Converts a sequence of `1`s and `0`s to a number.
 func toInt(_ bits: Bits) -> Int {
     return bits.reduce(0) { $0 << 1 + Int($1) }
 }
 
+/// Converts a sequence of `1`s and `0`s to a number.
 func toInt(_ bits: Bits.SubSequence) -> Int {
     return bits.reduce(0) { $0 << 1 + Int($1) }
 }
 
+/// Converts a sequence of `0-9A-F` characters to a sequence of `1`s and `0`s.
 func toBitsSubSequence(hexSequence: String) -> Bits.SubSequence {
     var bits: Bits = []
     for hex in hexSequence {
-        /// If the asciiValue fails to unwrap, that means the input is incorrect.
         var fourBitInt = UInt8(String(hex), radix: 16)!
-        /// Grab every bit in the sequence and append it to the `bits` array.
+        /// Grab every bit in the integer and append it to the `bits` array.
         for _ in 1...4 {
             let bit = (fourBitInt & 8) >> 3
             bits.append(bit)
@@ -33,53 +38,46 @@ func toBitsSubSequence(hexSequence: String) -> Bits.SubSequence {
     return bits[bits.startIndex..<bits.endIndex]
 }
 
+/// Returns an individual "bit" from a sequence of bits.
 func getBit(in bits: Bits.SubSequence, at: Int) -> UInt8 {
     let index = bits.index(bits.startIndex, offsetBy: at)
     return bits[index]
 }
 
-func getBits(in bits: Bits.SubSequence, at: Int, length: Int) -> Bits.SubSequence {
+/// Returns a subsequence of bits from a sequence of bits, starting from
+/// `at` and finishing at `at + length`.
+func getBits(
+    in bits: Bits.SubSequence,
+    at: Int,
+    length: Int
+) -> Bits.SubSequence {
     let indexStart = bits.index(bits.startIndex, offsetBy: at)
     let indexEnd = bits.index(bits.startIndex, offsetBy: at + length)
     return bits[indexStart..<indexEnd]
 }
 
+/// Returns a subsequence of bits from a sequence of bits, starting from
+/// `from` and finishing at the end of the array.
 func getBits(in bits: Bits.SubSequence, from: Int) -> Bits.SubSequence {
     let indexStart = bits.index(bits.startIndex, offsetBy: from)
     return bits[indexStart..<bits.endIndex]
 }
 
-func printBits(_ bits: Bits.SubSequence) {
-    print(bits.reduce("") { $0 + String($1) })
-}
-
-func printPacket(_ packet: Packet) {
-    print("{")
-    print("\"version\": \(packet.version),")
-    if packet.literalValue != nil {
-        print("\"literal\": \(packet.literalValue ?? -1)")
-    } else {
-        print("\"subPacket\": [")
-        for (i, subPacket) in packet.subPackets.enumerated() {
-            printPacket(subPacket)
-            if i != packet.subPackets.count - 1 {
-                print(",")
-            }
-        }
-        print("]")
-    }
-    print("}")
-}
-
+/// Returns `true` if the sequence of bits all contain `0`s.
 func isPadding(bits: Bits.SubSequence) -> Bool {
     return !bits.contains(1)
 }
 
-func readLiteralValue(in bits: Bits.SubSequence) -> (literalValue: Int, endIndex: Int) {
+/// Returns the literal value and the number of bits it took to read
+/// the value from a sequence of bits.
+func readLiteralValue(
+    in bits: Bits.SubSequence
+) -> (literalValue: Int, bitLength: Int) {
     var index = 0
     var literalBits: Bits = []
     while (true) {
-        literalBits.append(contentsOf: getBits(in: bits, at: index + 1, length: 4))
+        let groupBits = getBits(in: bits, at: index + 1, length: 4)
+        literalBits.append(contentsOf: groupBits)
         
         let isLastGroup = getBit(in: bits, at: index) == 0
         if (isLastGroup) {
@@ -89,42 +87,75 @@ func readLiteralValue(in bits: Bits.SubSequence) -> (literalValue: Int, endIndex
     }
     return (
         literalValue: toInt(literalBits),
-        endIndex: index + 5
+        bitLength: index + 5
     )
 }
 
-func readOperator(in bits: Bits.SubSequence) -> (subPackets: [Packet], endIndex: Int) {
+/// Returns a list of sub-packets from a sequence of bits, including
+/// the number of bits it took to read all the sub-packets.
+func readSubPackets(
+    in bits: Bits.SubSequence
+) -> (subPackets: [Packet], bitLength: Int) {
     let lengthTypeId = getBit(in: bits, at: 0)
+    var totalSubPacketBitLength = 0
+    var subPackets: [Packet] = []
     if (lengthTypeId == 0) {
-        let totalBitLengthOfSubPackets = toInt(getBits(in: bits, at: 1, length: 15))
-        var endIndex = 16
-        let subPacketBits = getBits(in: bits, at: endIndex, length: totalBitLengthOfSubPackets)
-        let (subPackets, subPacketsEndIndex) = readPackets(in: subPacketBits)
-        endIndex += subPacketsEndIndex
-        return (subPackets: subPackets, endIndex: endIndex)
-    } else {
-        let totalCountOfSubPackets = toInt(getBits(in: bits, at: 1, length: 11))
-        var endIndex = 12
-        var subPackets: [Packet] = []
-        for _ in 1...totalCountOfSubPackets {
-            let subPacketBits = getBits(in: bits, from: endIndex)
-            let (packet, subPacketEndIndex) = readPacket(in: subPacketBits)
-            subPackets.append(packet)
-            endIndex += subPacketEndIndex
+        /// If the length-type-ID is 0, that means the next 15 bits in the
+        /// sequence represents the total number of bits that the
+        /// sub-packets consists of.
+        let bitLengthOfSubPackets = toInt(getBits(in: bits, at: 1, length: 15))
+        totalSubPacketBitLength = 1 + 15
+        var subPacketBits = getBits(
+            in: bits,
+            at: 1 + 15,
+            length: bitLengthOfSubPackets
+        )
+        while(true) {
+            if (isPadding(bits: subPacketBits)) {
+                break
+            }
+            let (subPacket, subPacketBitLength) = readPacket(in: subPacketBits)
+            subPackets.append(subPacket)
+            totalSubPacketBitLength += subPacketBitLength
+            subPacketBits = getBits(in: subPacketBits, from: subPacketBitLength)
         }
-        return (subPackets: subPackets, endIndex: endIndex)
+    } else {
+        /// If the length-type-ID is 1, that means the next 11 bits in the
+        /// sequence represent the total number of sub-packets we
+        /// expect to read next.
+        let totalCountOfSubPackets = toInt(getBits(in: bits, at: 1, length: 11))
+        totalSubPacketBitLength = 1 + 11
+        var subPacketBits = getBits(in: bits, from: 1 + 11)
+        for _ in 1...totalCountOfSubPackets {
+            let (subPacket, subPacketBitLength) = readPacket(in: subPacketBits)
+            subPackets.append(subPacket)
+            totalSubPacketBitLength += subPacketBitLength
+            subPacketBits = getBits(in: subPacketBits, from: subPacketBitLength)
+        }
     }
+    return (
+        subPackets: subPackets,
+        bitLength: totalSubPacketBitLength
+    )
 }
 
-func readPacket(in bits: Bits.SubSequence) -> (packet: Packet, endIndex: Int) {
+/// Returns a packet from a sequence of bits, including the number of
+/// bits it took to read the entire packet, including sub-packets and
+/// literal values.
+func readPacket(in bits: Bits.SubSequence) -> (packet: Packet, bitLength: Int) {
+    /// The first 6 bits of the packet are header values.
     let version = toInt(getBits(in: bits, at: 0, length: 3))
     let typeId = toInt(getBits(in: bits, at: 3, length: 3))
-    var endIndex = 6
+    /// The rest of the bits for the packet contains the content.
+    let headerBitLength = 6
     
+    /// If the typeId is `4`, that means the packet is a literal value.
     if (typeId == 4) {
         /// Packet is a "literal value".
-        let (literalValue, literalValueEndIndex) = readLiteralValue(in: getBits(in: bits, from: endIndex))
-        endIndex += literalValueEndIndex
+        let literalValueBits = getBits(in: bits, from: headerBitLength)
+        let (literalValue, literalValueBitLength) = readLiteralValue(
+            in: literalValueBits
+        )
         return (
             packet: Packet(
                 version: version,
@@ -132,71 +163,70 @@ func readPacket(in bits: Bits.SubSequence) -> (packet: Packet, endIndex: Int) {
                 literalValue: literalValue,
                 subPackets: []
             ),
-            endIndex: endIndex
-        )
-    } else {
-        let (subPackets, operatorEndIndex) = readOperator(in: getBits(in: bits, from: endIndex))
-        endIndex += operatorEndIndex
-        return (
-            Packet(
-                version: version,
-                typeId: typeId,
-                literalValue: nil,
-                subPackets: subPackets
-            ),
-            endIndex: endIndex
+            bitLength: headerBitLength + literalValueBitLength
         )
     }
-}
-
-func readPackets(in bits: Bits.SubSequence) -> (packets: [Packet], endIndex: Int) {
-    var leftoverBits = bits
-    var totalEndIndex = 0
-    var packets: [Packet] = []
-    while(true) {
-        if (isPadding(bits: leftoverBits)) {
-            break
-        }
-        let (packet, endIndex) = readPacket(in: leftoverBits)
-        packets.append(packet)
-        totalEndIndex += endIndex
-        leftoverBits = getBits(in: leftoverBits, from: endIndex)
-    }
+    
+    /// Otherwise, if the typeId is something else, that means it has a
+    /// sequence of sub-packets.
+    let subPacketBits = getBits(in: bits, from: headerBitLength)
+    let (subPackets, subPacketsBitLength) = readSubPackets(in: subPacketBits)
     return (
-        packets: packets,
-        endIndex: totalEndIndex
+        Packet(
+            version: version,
+            typeId: typeId,
+            literalValue: nil,
+            subPackets: subPackets
+        ),
+        bitLength: headerBitLength + subPacketsBitLength
     )
 }
 
-func versionSum(packet: Packet) -> Int {
+/// Sums the versions for all the packets, including the sub-packets within
+/// the packet.
+func versionSumOfPacketAndSubPackets(_ packet: Packet) -> Int {
     var totalSum = packet.version
     for subPacket in packet.subPackets {
-        totalSum += versionSum(packet: subPacket)
+        totalSum += versionSumOfPacketAndSubPackets(subPacket)
     }
     return totalSum
 }
 
-func calculatePacket(packet: Packet) -> Int {
+/// Performs the operations within a packet and returns the value
+/// of performing those operations.
+func calculatePacket(_ packet: Packet) -> Int {
     if (packet.typeId == 0) {
-        return packet.subPackets.map { calculatePacket(packet: $0)}.reduce(0, +)
+        /// Operation to sum all the sub-packet values together.
+        return packet.subPackets.map { calculatePacket($0)}.reduce(0, +)
     } else if (packet.typeId == 1) {
-        return packet.subPackets.map { calculatePacket(packet: $0)}.reduce(1, *)
+        /// Operation to multiply all the sub-packet values together.
+        return packet.subPackets.map { calculatePacket($0)}.reduce(1, *)
     } else if (packet.typeId == 2) {
-        return packet.subPackets.map { calculatePacket(packet: $0)}.min()!
+        /// Operation to get the minimum value of all the sub-packet values.
+        return packet.subPackets.map { calculatePacket($0)}.min()!
     } else if (packet.typeId == 3) {
-        return packet.subPackets.map { calculatePacket(packet: $0)}.max()!
+        /// Operation to get the maximum value of all the sub-packet values.
+        return packet.subPackets.map { calculatePacket($0)}.max()!
     } else if (packet.typeId == 4) {
+        /// If the typeId is a literalValue, then no operations are required.
         return packet.literalValue!
     } else if (packet.typeId == 5) {
-        let calculatedPackets = packet.subPackets.map { calculatePacket(packet: $0)}
+        /// Returns `1` if the first sub-packet is larger than the second
+        /// sub-packet, otherwise it returns `0`.
+        let calculatedPackets = packet.subPackets.map { calculatePacket($0)}
         return calculatedPackets.first! > calculatedPackets.last! ? 1 : 0
     } else if (packet.typeId == 6) {
-        let calculatedPackets = packet.subPackets.map { calculatePacket(packet: $0)}
+        /// Returns `1` if the first sub-packet is smaller than the second
+        /// sub-packet, otherwise it returns `0`.
+        let calculatedPackets = packet.subPackets.map { calculatePacket($0)}
         return calculatedPackets.first! < calculatedPackets.last! ? 1 : 0
     } else if (packet.typeId == 7) {
-        let calculatedPackets = packet.subPackets.map { calculatePacket(packet: $0)}
+        /// Returns `1` if the first sub-packet is equal to the second
+        /// sub-packet, otherwise it returns `0`.
+        let calculatedPackets = packet.subPackets.map { calculatePacket($0)}
         return calculatedPackets.first! == calculatedPackets.last! ? 1 : 0
     }
+    /// This should be an invalid state.
     return 0
 }
 
@@ -206,11 +236,12 @@ enum Script {
         let packetHex = try readFileInCwd(file: "/Day-16-Input.txt")
             .trimmingCharacters(in: .whitespacesAndNewlines)
         
+        /// First we convert everything to ones and zeros, to make array slicing easier for us.
         let packetBits = toBitsSubSequence(hexSequence: packetHex)
+
         let packet = readPacket(in: packetBits).packet
         
-        print("Part 1: ", versionSum(packet: packet))
-        
-        print("Part 2: ", calculatePacket(packet: packet))
+        print("Part 1: ", versionSumOfPacketAndSubPackets(packet))
+        print("Part 2: ", calculatePacket(packet))
     }
 }
