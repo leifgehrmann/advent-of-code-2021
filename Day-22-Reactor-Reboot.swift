426a9171d2265deb877592d872fd85c932cf3e94
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-12-27 15:41:17 +0100

    Day-22 - Finished and cleaned up the solution so that it solves parts 1 and 2. Takes ~3.5 minutes.

--- Day-22-Reactor-Reboot.swift
+++ Day-22-Reactor-Reboot.swift
@@ -1,98 +1,5 @@
 import Foundation
 
-struct Box: Equatable {
-    let x: Range
-    let y: Range
-    let z: Range
-    
-    func intersects(box: Box) -> Bool {
-        return (x.intersects(range: box.x) &&
-            y.intersects(range: box.y) &&
-            z.intersects(range: box.z)
-        )
-    }
-    
-    func volume() -> Int {
-        return x.length() * y.length() * z.length()
-    }
-    
-    func merge(_ box: Box) -> [Box] {
-        var allIntersectX = true
-        var newXBoxes: [Box] = []
-        for xRange in x.split(range: box.x) {
-            var allIntersectY = true
-            var newYBoxes: [Box] = []
-            for yRange in y.split(range: box.y) {
-                var allIntersectZ = true
-                var newZBoxes: [Box] = []
-                for zRange in z.split(range: box.z) {
-                    let newBox = Box(
-                        x: xRange,
-                        y: yRange,
-                        z: zRange
-                    )
-                    if (
-                        newBox.intersects(box: self) ||
-                        newBox.intersects(box: box)
-                    ) {
-                        newZBoxes.append(newBox)
-                    } else {
-                        allIntersectZ = false
-                        allIntersectY = false
-                        allIntersectX = false
-                    }
-                }
-                
-                if (allIntersectZ) {
-                    newZBoxes = [
-                        Box(x: xRange, y: yRange, z: box.z.merge(range: self.z))
-                    ]
-                }
-                
-                newYBoxes.append(contentsOf: newZBoxes)
-            }
-            
-            if (allIntersectY) {
-                newYBoxes = [
-                    Box(x: xRange, y: box.y.merge(range: self.y), z: box.z.merge(range: self.z))
-                ]
-            }
-            
-            newXBoxes.append(contentsOf: newYBoxes)
-        }
-        
-        if (allIntersectX) {
-            return [
-                Box(x: box.x.merge(range: self.x), y: box.y.merge(range: self.y), z: box.z.merge(range: self.z))
-            ]
-        }
-        
-        return newXBoxes
-    }
-    
-    func subtract(_ box: Box) -> [Box] {
-        var newBoxes: [Box] = []
-        for xRange in x.split(range: box.x) {
-            for yRange in y.split(range: box.y) {
-                for zRange in z.split(range: box.z) {
-                    let newBox = Box(
-                        x: xRange,
-                        y: yRange,
-                        z: zRange
-                    )
-                    if (
-                        newBox.intersects(box: self) &&
-                        !newBox.intersects(box: box)
-                    ) {
-                        newBoxes.append(newBox)
-                    }
-                }
-            }
-        }
-        return newBoxes
-    }
-}
-
 struct Range: Hashable {
     let min: Int
     let max: Int
@@ -105,6 +12,10 @@ struct Range: Hashable {
         )
     }
     
+    func contains(range: Range) -> Bool {
+        return range.min >= min && range.max <= max
+    }
+    
     func contains(element: Int) -> Bool {
         return element >= min && element <= max
     }
@@ -114,10 +25,11 @@ struct Range: Hashable {
     }
     
     func split(range: Range) -> [Range] {
+        let newSelf = self
         if self.intersects(range: range) {
             // identical
             if range == self {
-                return [self]
+                return [newSelf]
             }
             // envelopes
             if range.min < self.min && range.max > self.max {
@@ -161,7 +73,7 @@ struct Range: Hashable {
             // touches left edge x2
             if range.min == self.min && range.max > self.max {
                 return [
-                    Range(min: range.min, max: range.max),
+                    Range(min: range.min, max: self.max),
                     Range(min: self.max + 1, max: range.max)
                 ]
             }
@@ -182,7 +94,7 @@ struct Range: Hashable {
             // single unit length
             if self.min == self.max && range.min == self.min {
                 return [
-                    self,
+                    newSelf,
                     Range(min: range.min + 1, max: range.max)
                 ]
             }
@@ -190,11 +102,11 @@ struct Range: Hashable {
             if self.min == self.max && range.max == self.max {
                 return [
                     Range(min: range.min, max: range.max - 1),
-                    self
+                    newSelf
                 ]
             }
         }
-        return [self, range]
+        return [newSelf, range]
     }
     
     func merge(range: Range) -> Range {
@@ -204,6 +116,130 @@ struct Range: Hashable {
     func subtract(range: Range) -> [Range] {
         self.split(range: range).filter {!$0.intersects(range: range)}
     }
+}
+
+struct Box: Equatable {
+    let x: Range
+    let y: Range
+    let z: Range
+    
+    func intersects(box: Box) -> Bool {
+        return (x.intersects(range: box.x) &&
+            y.intersects(range: box.y) &&
+            z.intersects(range: box.z)
+        )
+    }
+    
+    func contains(box: Box) -> Bool {
+        return (
+            x.contains(range: box.x) &&
+            y.contains(range: box.y) &&
+            z.contains(range: box.z)
+        )
+    }
+    
+    func volume() -> Int {
+        return x.length() * y.length() * z.length()
+    }
+    
+    func merge(_ box: Box) -> [Box] {
+        if self.contains(box: box) {
+            return [self]
+        }
+        if box.contains(box: self) {
+            return [box]
+        }
+        
+        var newBoxes: [Box] = []
+        for xRange in x.split(range: box.x) {
+            for yRange in y.split(range: box.y) {
+                for zRange in z.split(range: box.z) {
+                    let newBox = Box(
+                        x: xRange,
+                        y: yRange,
+                        z: zRange
+                    )
+                    if (
+                        newBox.intersects(box: self) ||
+                        newBox.intersects(box: box)
+                    ) {
+                        newBoxes.append(newBox)
+                    }
+                }
+
+            }
+        }
+        
+        /// Simplify the problem by generalizing the merge to be the current
+        /// box + all other boxes. This reduces the number of operations we
+        /// need to do when merging in `getBoxesTurnedOn()`.
+        newBoxes = newBoxes.filter { !self.intersects(box: $0) }
+        newBoxes.append(self)
+        
+        return newBoxes
+    }
+    
+    func subtract(_ box: Box) -> [Box] {
+        var newBoxes: [Box] = []
+        for xRange in x.split(range: box.x) {
+            for yRange in y.split(range: box.y) {
+                for zRange in z.split(range: box.z) {
+                    let newBox = Box(
+                        x: xRange,
+                        y: yRange,
+                        z: zRange
+                    )
+                    if (
+                        newBox.intersects(box: self) &&
+                        !newBox.intersects(box: box)
+                    ) {
+                        newBoxes.append(newBox)
+                    }
+                }
+            }
+        }
+        return newBoxes
+    }
+}
+
+func getBoxesTurnedOn(steps: [Step]) -> [Box] {
+    var boxes: [Box] = []
+    var boxesToMerge: [Box] = []
+    for (stepIndex, step) in steps.enumerated() {
+        if stepIndex % 5 == 0 {
+            print("\(stepIndex) / \(steps.count) Complete")
+        }
+        if step.action == .Off {
+            let boxToSubtract = step.box
+            var newBoxes: [Box]  = []
+            for (boxIndex, box) in boxes.enumerated().reversed() {
+                if box.intersects(box: boxToSubtract) {
+                    boxes.remove(at: boxIndex)
+                    newBoxes.append(contentsOf: box.subtract(boxToSubtract))
+                }
+            }
+            boxes.append(contentsOf: newBoxes)
+        } else {
+            boxesToMerge.append(step.box)
+        }
+        while boxesToMerge.count > 0 {
+            let boxToMerge = boxesToMerge.removeFirst()
+            var intersected = false
+            for (boxIndex, box) in boxes.enumerated().reversed() {
+                if box.intersects(box: boxToMerge) {
+                    intersected = true
+                    boxes.remove(at: boxIndex)
+                    let newBoxesToMerge = box.merge(boxToMerge)
+                    boxesToMerge.append(contentsOf: newBoxesToMerge)
+                    break
+                }
+            }
+            if !intersected {
+                boxes.append(boxToMerge)
+            }
+        }
+    }
+    return boxes
 }
 
 enum Action {
@@ -257,97 +293,14 @@ enum Script {
             .split(separator: "\n")
             .map { parseStep(line: String($0)) }
         
-        // Test that intersects works correctly
-//        print(Range(min: 2, max: 4).contains(element: 1)) // return false
-//        print(Range(min: 2, max: 4).contains(element: 2)) // return true
-//        print(Range(min: 2, max: 4).contains(element: 3)) // return true
-//        print(Range(min: 2, max: 4).contains(element: 4)) // return true
-//        print(Range(min: 2, max: 4).contains(element: 5)) // return false
-//        print(Range(min: 2, max: 2).contains(element: 1)) // return false
-//        print(Range(min: 2, max: 2).contains(element: 2)) // return true
-//        print(Range(min: 2, max: 2).contains(element: 3)) // return false
+        let within50Range = Range(min: -50, max: 50)
+        let within50Box = Box(x: within50Range, y: within50Range, z: within50Range)
+        let stepsWithin50 = steps.filter { within50Box.contains(box: $0.box) }
         
-        // Test that intersects works correctly
-//        print(Range(min: 2, max: 4).intersects(range: Range(min: 3, max: 4))) // return true
-//        print(Range(min: 2, max: 4).intersects(range: Range(min: 4, max: 4))) // return true
-//        print(Range(min: 2, max: 4).intersects(range: Range(min: 5, max: 5))) // return false
-//        print(Range(min: 2, max: 2).intersects(range: Range(min: 1, max: 5))) // return true
-//        print(Range(min: 2, max: 2).intersects(range: Range(min: 1, max: 1))) // return false
-//        print(Range(min: 2, max: 2).intersects(range: Range(min: 1, max: 2))) // return true
-//        print(Range(min: 2, max: 4).intersects(range: Range(min: 1, max: 3))) // return true
-//
-//        // Test that length works correctly
-//        print(Range(min: 2, max: 2).length()) // return 1
-//        print(Range(min: 2, max: 3).length()) // return 1
-//
-//        // Test that split works correctly
-//        print(Range(min: 2, max: 4).split(range: Range(min: 3, max: 4))) // 2 ranges, 2-2, 3-4
-//        print(Range(min: 2, max: 4).split(range: Range(min: 4, max: 4))) // 2 ranges, 2-2, 4-4
-//        print(Range(min: 1, max: 3).split(range: Range(min: 5, max: 7))) // 2 ranges, 1-3, 5-7
-//        print(Range(min: 1, max: 7).split(range: Range(min: 3, max: 5))) // 3 ranges, 1-2, 3-5, 6-7
-//        print(Range(min: 1, max: 3).split(range: Range(min: 1, max: 2))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 3).split(range: Range(min: 3, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 3).split(range: Range(min: 1, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 3, max: 3).split(range: Range(min: 1, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 1).split(range: Range(min: 1, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 4).split(range: Range(min: 2, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 2).split(range: Range(min: 3, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 2).split(range: Range(min: 3, max: 4))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 1, max: 3).split(range: Range(min: 2, max: 4))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 2, max: 4).split(range: Range(min: 1, max: 3))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: -21, max: 9).split(range: Range(min: -35, max: 9))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 9, max: 21).split(range: Range(min: 9, max: 35))) // 3 ranges, 1-2, 3-3
-//        print(Range(min: 8, max: 21).split(range: Range(min: 9, max: 35))) // 3 ranges, 1-2, 3-3
-//        return
-        
-//        print(Range(min: 2, max: 4).subtract(range: Range(min: 1, max: 3))) // 4-4
-//        print(Range(min: 2, max: 4).subtract(range: Range(min: 3, max: 3))) // 2-2, 4-4
-//        print(Range(min: 2, max: 4).subtract(range: Range(min: 4, max: 4))) // 2-3
-//
-//        print(Box(x: Range(min: 1, max: 3), y: Range(min: 1, max: 3), z: Range(min: 1, max: 3)).subtract(
-//            Box(x: Range(min: 2, max: 2), y: Range(min: 2, max: 2), z: Range(min: 2, max: 2))
-//        ))
-        
-        var boxes: [Box] = []
-        var boxesToMerge: [Box] = []
-        for step in steps {
-            print(step)
-            if step.action == .Off {
-                let boxToSubtract = step.box
-                var newBoxes: [Box]  = []
-                for (boxIndex, box) in boxes.enumerated().reversed() {
-                    if box.intersects(box: boxToSubtract) {
-                        boxes.remove(at: boxIndex)
-                        newBoxes.append(contentsOf: box.subtract(boxToSubtract))
-                    }
-                }
-                boxes.append(contentsOf: newBoxes)
-            } else {
-                boxesToMerge.append(step.box)
-            }
-            while boxesToMerge.count > 0 {
-                let boxToMerge = boxesToMerge.removeFirst()
-                var intersected = false
-                for (boxIndex, box) in boxes.enumerated().reversed() {
-                    if box.intersects(box: boxToMerge) {
-                        intersected = true
-                        print(box)
-                        print(boxToMerge)
-                        boxes.remove(at: boxIndex)
-                        let newBoxesToMerge = box.merge(boxToMerge)
-                        print(newBoxesToMerge)
-                        print(boxes.count)
-                        boxesToMerge.append(contentsOf: newBoxesToMerge)
-                        break
-                    }
-                }
-                if !intersected {
-                    boxes.append(boxToMerge)
-                }
-            }
-        }
-        
-        print(boxes)
-        print(boxes.reduce(0) {$0 + $1.volume()}) // Expect 26
+        let boxesTurnedOnWithin50 = getBoxesTurnedOn(steps: stepsWithin50)
+        print("Part 1: ",  boxesTurnedOnWithin50.reduce(0) {$0 + $1.volume()})
+
+        let boxesTurnedOn = getBoxesTurnedOn(steps: steps)
+        print("Part 2: ",  boxesTurnedOn.reduce(0) {$0 + $1.volume()})
     }
 }
